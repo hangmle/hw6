cb2ab9b00918004852e0a5cd984c61d04b711590
Hang Le <hang.le01@student.csulb.edu>
2022-10-12 19:13:39 -0700

    ex3

--- js/ex3.js
+++ js/ex3.js
@@ -1,2 +1,12 @@
 // example 3
 
+// const s = document.querySelector("p")
+
+const newStyle = () => {
+  for (let i=0; i < document.getElementsByTagName("p").length; i++) {
+    document.getElementsByTagName("p")[i].style.backgroundColor = "yellow";
+    document.getElementsByTagName("p")[i].style.fontWeight = "700";
+  }
+}
+
+newStyle();
