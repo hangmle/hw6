b4be3c0fc3402bf5c2b09f8929baf8e1dd45e605
Hang Le <hang.le01@student.csulb.edu>
2022-10-13 16:31:24 -0700

    7

--- js/ex7.js
+++ js/ex7.js
@@ -9,20 +9,21 @@ const n2Int = Number(n2);
 // if (Number.isInteger(n1Int) && Number.isInteger(n2Int) && n1Int > 0 && n2Int > n1Int) {
 //   console.log(`The sum of the numbers starting with "${n1}" and ending with "${n2}" is ${sumNotDiv3or4(n1Int, n2Int)}`);
 // } else {
-//   console.log("error")
+//   console.log(`Error! You must enter integers. You entered "${n1}" and "${n2}". Try again.`)
 // }
-
-spanEl.textContent[0] = n1;
-document.getElementById("content")[0].appendChild(n1);
-spanEl.textContent[1] = n2;
-document.getElementById("content")[1].appendChild(n2);
-
 
 const display = () => {
   for (let i=0; i < 2; i++) {
     const spanEl = document.createElement("span");
-    document.getElementsByTagName("span")[i].style.color = "red";
-    document.getElementsByTagName("span")[i].style.fontWeight = "700";
+    document.getElementById("content")[0].appendChild(n1);
+    document.getElementById("content")[1].appendChild(n2);
+    if (Number.isInteger(n1Int) && Number.isInteger(n2Int)) {
+      document.getElementsByTagName("span")[i].style.color = "red";
+      document.getElementsByTagName("span")[i].style.fontWeight = "700";
+    } else {
+      console.error("Error!");
+      console.log(`You must enter integers. You entered "${n1}" and "${n2}". Try again.`)
+    }
   }  
 }
 
