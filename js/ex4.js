f7979df72e1f28d1aac7476d8599ed19f2fb1eac
Hang Le <hang.le01@student.csulb.edu>
2022-10-12 21:12:50 -0700

    commit

--- js/ex4.js
+++ js/ex4.js
@@ -1,2 +1,13 @@
 // example 4
 
+const addLink = () => {
+  const newLink = document.createElement("li");
+  newLink.id = "cob"
+  newLink.textContent = "College of Business"
+  document.getElementById("links").insertBefore(newLink, document.getElementById("is"));
+  document.getElementById("cob").href = "https://www.csulb.edu/college-of-business";
+}
+
+addLink();
+
+// NOT COMPLETED
--- js/ex5.js
+++ js/ex5.js
@@ -1,2 +1,10 @@
 // example 5
 
+for (i=1; i<=12; i++) {
+  const number = document.createElement("p");
+  // number.id = "num"
+  number.textContent = i;
+  document.getElementById("output").appendChild(number);
+}
+
+// NOT COMPLETED
