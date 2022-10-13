1c5c0faaeba4c4d4026b334b7860a3201040bd94
Hang Le <hang.le01@student.csulb.edu>
2022-10-12 18:52:29 -0700

    ex2

---
+++ js/ex2.js
@@ -0,0 +1,11 @@
+// example 2
+
+// Display text for 400 level courses
+
+const displayText400 = () => {
+  for (let i =  0; i < document.getElementsByClassName("400level").length; i++) {
+    console.log(document.getElementsByClassName("400level")[i].textContent);
+  }
+}
+
+displayText400();
